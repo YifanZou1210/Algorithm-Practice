a4a8a2899cddcccfe69405b9c22ec9bf23caa2ca
Yifan Zou <zou.yifa@northeastern.edu>
2025-04-23 23:42:40 -0400

    finish initialization of jupyter notebook architecture

--- LCPR/.lcpr/687.longest-univalue-path.py
+++ LCPR/.lcpr/687.longest-univalue-path.py
@@ -12,7 +12,6 @@
 # Definition for a binary tree node.
 from collections import deque
 from typing import Optional
-
 class TreeNode:
     def __init__(self, val: int = 0, left: Optional['TreeNode'] = None, right: Optional['TreeNode'] = None):
         self.val = val
