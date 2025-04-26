44a2a0009f536b046bf20b8854f7e60f47cfe4e8
Yifan Zou <zou.yifa@northeastern.edu>
2025-04-26 12:04:23 -0400

    finished all version of 2 sum solution

--- leetcode/1.两数之和.py
+++ leetcode/1.两数之和.py
@@ -7,6 +7,7 @@ from typing import Optional
 from collections import List, defaultdict
 # @lc code=start
 
+# * 1. 字典解法
 class Solution_Hashmap:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         dict = defaultdict(int)
@@ -14,14 +15,22 @@ class Solution_Hashmap:
             if target-num in dict.keys():
                 return [i, dict[target-num]]
             dict[num] = i
-
-class Solution2:
+# TC: O(n), 只遍历一次数组，每次操作哈希表是O(1)总共n次操作
+# SC: O(n),最坏情况下，字典存下真个个数组中的每个元素和下标
+# * 2. 暴力解法
+class Solution_Brute_Force:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        pass
+        for i in range(len(nums)):
+            for j in range(i+1, len(nums)):
+                if nums[i]+nums[j] == target:
+                    return [i, j]
+# TC: O(n^2) - 外层循环n，内层循环worst condition是n-1,n-2,...总共比较次数是 n(n-1)/2即 O(n^2)
+# SC: O(1) - 没有使用额外数据结构仅用了临时变量i, j占用的是常量空间             
 
+#! 总Solution调用位置
 class Solution:
     def __init__(self):
-        self.solver = Solution_Hashmap()  
+        self.solver = Solution_Brute_Force()  
     def __getattr__(self, name):
         return getattr(self.solver, name)
 
